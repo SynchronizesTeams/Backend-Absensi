f3b1cae8fe45b08501cdd4d01ccbaaa2aa59af7f
adeli <adeli@mail.com>
2025-08-12 10:26:02 +0700

    change izin function

--- app/Http/Controllers/API/Absensi/AbsensiController.php
+++ app/Http/Controllers/API/Absensi/AbsensiController.php
@@ -133,6 +133,7 @@ class AbsensiController extends Controller
         $currentTime = $now->format('H:i');
         $today = $now->toDateString();
         $request->validate([
+            'keterangan_masuk' => 'required|string',
             'keterangan' => 'required|string',
             'photo_izin' => 'required|image|max:10240',
         ]);
@@ -147,7 +148,7 @@ class AbsensiController extends Controller
                 'tanggal' => $today,
                 'jam_masuk' => null,
                 'jam_pulang' => null,
-                'keterangan_masuk' => 'izin',
+                'keterangan_masuk' => $request->keterangan_masuk,
                 'keterangan_pulang' => null,
                 'photo_izin' => $path,
                 'keterangan' => $request->keterangan,
