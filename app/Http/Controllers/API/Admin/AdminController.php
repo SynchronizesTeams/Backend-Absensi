a9a46cba265e0f9224fe1d094326d7d0a983aa9d
adeli <adeli@mail.com>
2025-08-22 13:11:07 +0700

    Add admin user management

--- app/Http/Controllers/API/Admin/AdminController.php
+++ app/Http/Controllers/API/Admin/AdminController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\API\Admin;
 
 use App\Exports\AbsensiExport;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Maatwebsite\Excel\Facades\Excel;
 
 class AdminController extends Controller
@@ -25,5 +27,25 @@ class AdminController extends Controller
         }
 
         return Excel::download(new AbsensiExport($from, $to), $filename);
+    }
+
+    public function userCount()
+    {
+        return Cache::remember('user_count', now()->addHours(1), function () {
+            return response()->json([
+                'user_count' => User::count(),
+            ]);
+        });
+    }
+
+    public function userFilter($role)
+    {
+        return Cache::remember("user_count_by_role_{$role}", now()->addHours(1), function () use ($role) {
+            $users = User::where('role', '=', $role)->count();
+
+            return response()->json([
+                'users' => $users,
+            ]);
+        });
     }
 }
